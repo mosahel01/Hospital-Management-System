d1bec3e8caac52e97479fc84efa567b00afa052f
mosahel01 <mos.sahil01@gmail.com>
2026-02-14 07:58:22 +0530

    feat: add Insurance entity to domain model

---
+++ src/main/java/com/example/hospitalManagement/entity/Insurance.java
@@ -0,0 +1,37 @@
+package com.example.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String policyNumber;
+
+    @Column(nullable = false, length = 50)
+    private String provider;
+
+    @Column(nullable = false)
+    private LocalDate validUntil;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
