e6e9c1a13f9d583ad871806a596d99276885f308
mosahel01 <mos.sahil01@gmail.com>
2026-02-16 07:39:57 +0530

    feat: Add Department entity to domain model

---
+++ src/main/java/com/example/hospitalManagement/entity/Department.java
@@ -0,0 +1,35 @@
+package com.example.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(unique = true, nullable = false)
+    private Long head_doctor_id;
+
+
+
+}
