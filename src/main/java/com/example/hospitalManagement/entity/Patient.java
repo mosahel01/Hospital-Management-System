b036a48951fa8eb1dedcae80689d0f1c9c1b4ecd
mosahel01 <mos.sahil01@gmail.com>
2026-03-24 08:34:03 +0530

    style: format Patient.java for consistent indentation

--- src/main/java/com/example/hospitalManagement/entity/Patient.java
+++ src/main/java/com/example/hospitalManagement/entity/Patient.java
@@ -13,53 +13,37 @@ import java.util.List;
 @ToString
 @Getter
 @Setter
-@Table(
-        name = "patient",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "unique_patient_email", columnNames = {
-                        "email"
-                }),
-                @UniqueConstraint(name = "unique_patient_name_birth_date", columnNames = {
-                        "name", "birthDate"
-                })
-        },
-        indexes = {
-                @Index(name = "idx_patient_birthdate", columnList = "birthDate")
-        }
-
-)
+@Table(name = "patient", uniqueConstraints = {@UniqueConstraint(name = "unique_patient_email", columnNames = {"email"}), @UniqueConstraint(name = "unique_patient_name_birth_date", columnNames = {"name", "birthDate"})}, indexes = {@Index(name = "idx_patient_birthdate", columnList = "birthDate")})
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, length = 40)
-    private String name;
+	@Column(nullable = false, length = 40)
+	private String name;
 
-    //    @ToString.Exclude
-    private LocalDate birthDate;
+	@ToString.Exclude
+	private LocalDate birthDate;
 
-    @Column(unique = true, nullable = false)
-    private String email;
+	@Column(unique = true, nullable = false)
+	private String email;
 
-    private String gender;
+	private String gender;
 
-    @CreationTimestamp
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
 
-    @Enumerated(EnumType.STRING)
-    private BloodGroupType blood_group;
+	@Enumerated(EnumType.STRING)
+	private BloodGroupType blood_group;
 
-    @OneToOne
-    @JoinColumn(name = "insurance_id") // inverse side
-    private Insurance insurance;
+	@OneToOne
+	@JoinColumn(name = "insurance_id") // inverse side
+	private Insurance insurance;
 
-    @OneToMany(mappedBy = "patient")
-    private List<Appointment> appointmentList;
-
-
+	@OneToMany(mappedBy = "patient")
+	private List<Appointment> appointmentList;
 }
