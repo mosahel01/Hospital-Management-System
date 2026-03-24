889cdd8226291bcbeff7c5e56d6ddcf074b04c26
mosahel01 <mos.sahil01@gmail.com>
2026-03-24 08:34:11 +0530

    style: add comments for clarity in PatientRepository.java

--- src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
@@ -17,6 +17,11 @@ import java.util.List;
 // Spring Data JPA
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    // Spring Data JPA is smart.
+    // If you name a method following a specific pattern,
+    // it writes the SQL for you automatically.
+
+    // find the patient whose name matches exactly what I give you.
     Patient findByName(String name);
 
     List<Patient> findByBirthDateBetween(LocalDate birthDate, LocalDate birthDate2);
@@ -25,18 +30,26 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     List<Patient> findByNameContainingIgnoreCaseOrderByIdDesc(String name);
 
+    // Sometimes the method names get too long or the logic is complex.
+    // That’s when we use the @Query annotation to write the logic ourselves.
     @Query("SELECT p FROM Patient p where p.blood_group = ?1")
     List<Patient> findByBloodGroup(@Param("blood_group") BloodGroupType blood_group);
 
-    @Query("SELECT new com.example.hospitalManagement.dto.BloodGroupCountResponseEntity(p.blood_group,Count(p)) FROM Patient p group by p.blood_group")
+    @Query(
+            "SELECT new"
+                + " com.example.hospitalManagement.dto.BloodGroupCountResponseEntity(p.blood_group,Count(p))"
+                + " FROM Patient p group by p.blood_group")
     List<BloodGroupCountResponseEntity> countEachBloodGroupType();
 
+    // Notice the nativeQuery = true. This tells Spring: "Don't try to be smart;
+    // just run this exact SQL command directly against the database
+    // Pure "SQL"
     @Query(value = "SELECT * FROM Patient", nativeQuery = true)
     List<Patient> findAllPatients();
 
-    @Modifying
-    @Transactional
+    @Modifying // tells spring that this query will change data, not just read.
+    @Transactional // safety net. It ensures that if the update fails halfway through, it "rolls
+                   // back" so your database doesn't get corrupted.
     @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
     int updateNameById(@Param("name") String name, @Param("id") Long id);
-
 }
